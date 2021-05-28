5560d8c446f84440314772027a4213fb45c66dcb
Udara Damitha <udaradamitha@gmail.com>
2021-05-28 18:47:33 +0530

    finished sign in  and signup

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -44,7 +44,7 @@ class SignUp extends React.Component {
         confirmPassword: ''
       });
     } catch (error) {
-      console.error(error);
+      console.error('error in sign up', error);
     }
   };
 
